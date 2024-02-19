c033c1da0b011c38c83691cb15cb19e7b5d2b654
ramamimu <ahm.f.ramadhani@gmail.com>
2024-02-19 17:46:39 +0700

    feat: explisit intent

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,53 +1,51 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var edtWidth: EditText
-    private lateinit var edtHeight: EditText
-    private lateinit var edtLength: EditText
-    private lateinit var btnCalculate: Button
-    private lateinit var tvResult: TextView
-
-    companion object {
-        private const val STATE_RESULT = "state_result"
-    }
-
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        edtWidth = findViewById(R.id.edt_width)
-        edtHeight = findViewById(R.id.edt_height)
-        edtLength = findViewById(R.id.edt_length)
-        btnCalculate = findViewById(R.id.btn_calculate)
-        tvResult = findViewById(R.id.tv_result)
-        btnCalculate.setOnClickListener {
-            if (it?.id == R.id.btn_calculate) {
-                val inputLength = edtLength.text.toString().trim()
-                val inputWidth = edtWidth.text.toString().trim()
-                val inputHeight = edtHeight.text.toString().trim()
-                if (inputLength.isNotEmpty() and inputWidth.isNotEmpty() and inputHeight.isNotEmpty()) {
-                    val volume =
-                        inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
-                    tvResult.text = volume.toString()
-                }
-            }
-        }
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
 
-        if (savedInstanceState != null){
-            val result = savedInstanceState.getString(STATE_RESULT)
-            tvResult.text = result
-        }
+        val btnMoveActivityData: Button = findViewById(R.id.btn_move_activity_data)
+        btnMoveActivityData.setOnClickListener(this)
+
+        val btnMoveWithObject: Button = findViewById(R.id.btn_move_activity_object)
+        btnMoveWithObject.setOnClickListener(this)
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putString(STATE_RESULT, tvResult.text.toString())
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+            R.id.btn_move_activity_data -> {
+                val moveWithDataIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Rama")
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 99)
+                startActivity(moveWithDataIntent)
+            }
+            R.id.btn_move_activity_object -> {
+                val person = Person(
+                    "rama academy",
+                    5,
+                    "ahmad@gmail.com",
+                    "Surabaya"
+                )
+
+                val moveWithObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
+                moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
+                startActivity(moveWithObjectIntent)
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/myapplication/Person.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+import android.os.Parcel
+import android.os.Parcelable
+import android.provider.ContactsContract.CommonDataKinds.Email
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Person (
+    val name: String?,
+    val age: Int?,
+    val email: String?,
+    val city: String?,
+): Parcelable
